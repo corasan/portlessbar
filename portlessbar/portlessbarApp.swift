2658ba0bd0ac562801df4648e33fdcc403f77943
Henry Paulino <me@henrypl.com>
2026-03-12 17:58:10 +0000

    Initial Commit

---
+++ portlessbar/portlessbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  portlessbarApp.swift
+//  portlessbar
+//
+//  Created by Henry on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct portlessbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
